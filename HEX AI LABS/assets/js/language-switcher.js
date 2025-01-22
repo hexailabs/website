3ad9d0fdcdd46f5569fff74ad21686133255688e
hexailabs <hexailabs0403@gmail.com>
2025-01-22 13:46:35 +0900

    Delete HEX AI LABS directory

--- HEX AI LABS/assets/js/language-switcher.js
+++
@@ -1,9 +0,0 @@
-function changeLanguage(lang) {
-  if (lang === 'en') {
-    // 영어 페이지로 이동
-    window.location.href = 'index.html';
-  } else if (lang === 'ko') {
-    // 한국어 페이지로 이동
-    window.location.href = 'ko_index.html';
-  }
-}
